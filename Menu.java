2769bad97bbea368ad3a5901e0ed1ab9093af089
THE BEAST <gunyukwunny@gmail.com>
2024-10-17 11:57:41 +0200

    Updated Start Menu
    - button layout now scales with screen size
    - button size now scales with screen size
    - button text font size now scales with screen size
    - cleaned up code a bit (still looks messy because it's swing, but less)

--- Menu.java
+++ Menu.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -27,47 +28,66 @@ public class Menu {
     // button layout constraints. Used to configure the layout manager.
     GridBagConstraints constraints = new GridBagConstraints();
 
+    // Variables for scaling with screen size
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    // frame dimensions depending on screen size
+    float screenSizeMultiplier = 0.625f; // frame size compared to the screen size. Default: 0.625f
+    int frameWidth = (int) Math.round(screenSize.getWidth() * screenSizeMultiplier);
+    int frameHeight = (int) Math.round(screenSize.getHeight() * screenSizeMultiplier);
+    // button dimensions depending on screen size
+    int buttonWidth = (int) Math.round(screenSize.getWidth() * screenSizeMultiplier * 0.15);
+    int buttonHeight = (int) Math.round(screenSize.getHeight() * screenSizeMultiplier * 0.06);
+    Dimension buttonDimension = new Dimension(buttonWidth, buttonHeight);
+    int buttonFontSize = (int) Math.round(buttonWidth / 12);
+    // amount of pixels between buttons depending on screen size
+    int topInset = (int) Math.round(240 * screenSizeMultiplier);
+    int leftInset = (int) Math.round(16 * screenSizeMultiplier);
+    int bottomInset = (int) Math.round(16 * screenSizeMultiplier);
+    int rightInset = (int) Math.round(16 * screenSizeMultiplier);
 
     /**
      * Sets up the start menu screen.
      */
     public Menu() {
-        
+        System.out.println(buttonWidth + ", " + buttonFontSize);
         // frame init
         frame.setLayout(new GridBagLayout());
         //sets window size relative to user's screen size
-        frame.setSize((int) Math.round(screenSize.getWidth() / 1.6), 
-            (int) Math.round(screenSize.getHeight() / 1.6));
+        frame.setSize(frameWidth, frameHeight);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.getContentPane().setBackground(Mazesweeper.DARK_GREEN);
 
+        // GridBagConstraint config.
+        constraints.insets = new Insets(topInset, leftInset, bottomInset, rightInset);
+        constraints.fill = GridBagConstraints.BOTH;
 
         // start button init
         JButton startButton = new JButton("START");
         startButton.setBackground(Mazesweeper.LIGHT_GREEN);
         startButton.addActionListener(startListener);
-        constraints.insets = new Insets(150, 10, 10, 10);
+        startButton.setPreferredSize(buttonDimension);
+        startButton.setFont(new Font("Dialog", Font.BOLD, buttonFontSize));
         frame.add(startButton, constraints);
 
         // rules button init
         JButton rulesButton = new JButton("HOW TO PLAY");
         rulesButton.setBackground(Mazesweeper.LIGHT_GREEN);
         rulesButton.addActionListener(rulesListener);
-        constraints.insets.top = 10;
-        constraints.gridy = 1;
+        rulesButton.setPreferredSize(buttonDimension);
+        rulesButton.setFont(new Font("Dialog", Font.BOLD, buttonFontSize));
+        constraints.insets.top = bottomInset; // sets top inset to be same as other insets
+        constraints.gridy = 1; // gridy of start button is 0, rules button is 1 lower
         frame.add(rulesButton, constraints);
 
         // quit button init
         JButton quitButton = new JButton("QUIT");
         quitButton.setBackground(Mazesweeper.LIGHT_GREEN);
         quitButton.addActionListener(quitListener);
-        constraints.gridy = 2;
+        quitButton.setPreferredSize(buttonDimension);
+        quitButton.setFont(new Font("Dialog", Font.BOLD, buttonFontSize));
+        constraints.gridy = 2; // gridy of rules button is 1, quit button is 1 lower
         frame.add(quitButton, constraints);
-
-
-
         frame.setVisible(true);
     }
 
