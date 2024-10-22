7c893165c6f5e386a030a4f6bc35b540be62c757
THE BEAST <gunyukwunny@gmail.com>
2024-10-22 11:17:17 +0200

    Cleaned menu imports

--- Menu.java
+++ Menu.java
@@ -1,25 +1,20 @@
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.awt.Image;
+import java.awt.Insets;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
+import java.awt.event.ActionEvent; 
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-
-import java.awt.Graphics;
 import java.io.File;
-import java.awt.BorderLayout;
-import java.io.*;
-
-import javax.swing.ImageIcon;
+import java.io.IOException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 
 
